aa32c8783c220396ef1f13ec2d9dcc4ab02aac5d
Coralie-pixel <63093536+Coralie-pixel@users.noreply.github.com>
2020-04-03 14:45:00 +0200

    Push exo

---
+++ tp1/exercice_0.py
@@ -0,0 +1,12 @@
+a = 5.3
+b = 9.70
+c = a + b
+print(c)
+c += a
+d = c
+d -= 1
+# d = 19,3 "c'est un nombre à virgule (float)"
+# c = 20,3 "c'est un nombre à virgule (float)"
+c_int = int(c)
+print(c_int)
+# " On remarque que ca arrondi à l'entier inferieur "
